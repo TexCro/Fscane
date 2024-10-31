4031e2b9edc256fddcdd47081e94cbfa8a885f49
TexCro <156850678+TexCro@users.noreply.github.com>
2024-10-31 14:40:29 -0400

    Update logged.js

--- logged.js
+++ logged.js
@@ -1,1 +1,17 @@
+{
+    "tableData": [
+        {
+            "Current": "Current",
+            "Grower": "Grower",
+            "Pollen": "Pollen",
+            "Seed": "Seed"
+        },
+        {
+            "Current": 7653,
+             "Grower": "Andrew Grandlinard",
+            "Pollen": "1PRAP73",
+            "Seed": "1PFHM95"
+        }
 
+    ]
+}
