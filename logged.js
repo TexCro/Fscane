033573649a0d3940ad5ed07b6c18fd1cc4f38d34
TexCro <156850678+TexCro@users.noreply.github.com>
2024-10-31 14:51:26 -0400

    Update logged.js

--- logged.js
+++ logged.js
@@ -11,7 +11,14 @@
              "Grower": "Andrew Grandlinard",
             "Pollen": "1PRAP73",
             "Seed": "1PFHM95"
+        },
+        {
+            "Current": 9805,
+             "Grower": "Sandyland",
+            "Pollen": "4FKW1",
+            "Seed": "1PFHM95"
         }
+
 
     ]
 }
